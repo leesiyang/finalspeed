b0cc99ff1d76c45f81aefae3d26029186bad140e
leesiyang <mrlbz@hotmail.com>
2016-06-08 00:58:00 +0800

    Update install_fs.sh

--- install_fs.sh
+++ install_fs.sh
@@ -4,7 +4,7 @@ export PATH
 
 
 install_path=/fs/
-package_download_url=https://raw.githubusercontent.com/tutugreen/finalspeed/master/finalspeed_server.zip
+package_download_url=https://raw.githubusercontent.com/leesiyang/finalspeed/master/finalspeed_server.zip
 package_save_name=finalspeed_server.zip
  
 function checkjava(){
